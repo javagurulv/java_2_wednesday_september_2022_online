d2dde4c76bb7cd1bb09f6038e4bf4bd9ca3a7406
ArtjomsB <ar.bogatirjovs@gmail.com>
2022-09-12 18:13:11 +0300

    more explanation

--- team_fitness_club/src/main/java/fitness_club/FitnessClubApplication.java
+++ team_fitness_club/src/main/java/fitness_club/FitnessClubApplication.java
@@ -1,7 +1,46 @@
 package fitness_club;
 
+import java.util.Scanner;
+
 class FitnessClubApplication {
     public static void main(String[] args) {
+        while (true) {
+            menu();
+            int menuNumber = getMenuNumberFromUser();
+            executeSelectedMenuItem(menuNumber);
+        }
+    }
 
+    private static void menu() {
+        System.out.println("1 - Register");
+        System.out.println("2 - Sign in");
+        System.out.println("3 - Exit");
+    }
+
+    private static int getMenuNumberFromUser() {
+        System.out.println("Enter menu item number to execute:");
+        Scanner scanner = new Scanner(System.in);
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    private static void executeSelectedMenuItem(int selectedMenu) {
+        switch (selectedMenu) {
+            case 1 -> registerForm();
+            case 2 -> signInMenu();
+            case 3 -> exitProgramAction();
+        }
+    }
+
+    private static void registerForm() {
+        System.out.println("Registration success");
+    }
+
+    private static void signInMenu() {
+        System.out.println("You sign in as - ");
+    }
+
+    private static void exitProgramAction() {
+        System.out.println("Good by!");
+        System.exit(0);
     }
 }
