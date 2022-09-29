58a4b2974e7eacea635806a84eb94ecdb3bfec81
VsNicko <n.visnakovs@inbox.lv>
2022-09-29 22:18:35 +0300

    Changes by Arturs

--- team_clean_sour_cream/src/main/java/lv/javaguru/java2/rentapp/core/services/search_criterias/ConditionerCriteria.java
+++ team_clean_sour_cream/src/main/java/lv/javaguru/java2/rentapp/core/services/search_criterias/ConditionerCriteria.java
@@ -1,17 +1,36 @@
 package lv.javaguru.java2.rentapp.core.services.search_criterias;
 
+import lombok.Getter;
 import lv.javaguru.java2.rentapp.domain.MiniBus;
 import lv.javaguru.java2.rentapp.domain.PassengerCar;
 import lv.javaguru.java2.rentapp.domain.Vehicle;
 
+@Getter
 public class ConditionerCriteria implements SearchCriteria {
+
+    private String hasConditioner;
+
+    public ConditionerCriteria(String hasConditioner) {
+        this.hasConditioner = hasConditioner;
+    }
+
 
     @Override
     public boolean test(Vehicle vehicle) {
-        if (vehicle instanceof PassengerCar) {
-            return ((PassengerCar) vehicle).isAirConditioningAvailable();
-        } else if (vehicle instanceof MiniBus) {
-            return ((MiniBus) vehicle).isAirConditioningAvailable();
-        } else return false;
+        if (hasConditioner.equals("true")) {
+            if (vehicle instanceof PassengerCar) {
+                return ((PassengerCar) vehicle).isAirConditioningAvailable();
+            } else if (vehicle instanceof MiniBus) {
+                return ((MiniBus) vehicle).isAirConditioningAvailable();
+            }
+        } else if (hasConditioner.equals("false")) {
+            if (vehicle instanceof PassengerCar) {
+                return !((PassengerCar) vehicle).isAirConditioningAvailable();
+            } else if (vehicle instanceof MiniBus) {
+                return !((MiniBus) vehicle).isAirConditioningAvailable();
+            }
+        }
+        return false;
     }
 }
+
