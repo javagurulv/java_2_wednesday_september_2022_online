668d6099ec07be24d7bc369cc2c929475bea066c
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-10-01 00:45:52 +0300

    team_sour_cream v4 refactoring in progress 01

--- team_clean_sour_cream/src/main/java/lv/javaguru/java2/rentapp/console_UI/SearchVehicleUIAction.java
+++ team_clean_sour_cream/src/main/java/lv/javaguru/java2/rentapp/console_UI/SearchVehicleUIAction.java
@@ -3,12 +3,8 @@ package lv.javaguru.java2.rentapp.console_UI;
 import lv.javaguru.java2.rentapp.core.requests.SearchVehicleRequest;
 import lv.javaguru.java2.rentapp.core.responses.SearchVehicleResponse;
 import lv.javaguru.java2.rentapp.core.services.SearchVehicleService;
-import lv.javaguru.java2.rentapp.core.services.search_criterias.*;
-import lv.javaguru.java2.rentapp.domain.Vehicle;
 
-import java.awt.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -22,37 +18,36 @@ public class SearchVehicleUIAction implements UIAction {
 
     public void execute() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println(" ");
-        System.out.println("Choose vehicle type");
-        System.out.println("1. Passenger Car");
-        System.out.println("2. Mini Bus");
-        System.out.println("3. Motorcycle");
-        System.out.println("4. Car Trailer");
-        System.out.println(" ");
-        int vehicleTypeChoice = Integer.parseInt(scanner.nextLine());
 
+        System.out.println("Start searching by vehicle type first");
+
+        printVehicleTypesMenu();
+
+        int vehicleTypeChoice = Integer.parseInt(scanner.nextLine());
 
         switch (vehicleTypeChoice) {
 
             case 1 -> {
+
                 SearchVehicleRequest.SearchVehicleRequestBuilder searchVehicleRequestBuilder = SearchVehicleRequest.builder().vehicleType("PassengerCar");
 
                 boolean addAnotherCriteria = true;
-                List<String> criteria = new ArrayList<>();
-                String passengerAmount = "Passenger amount";
-                String doorsAmount = "Doors amount";
-                String baggageAmount = "Baggage amount";
-                String transmission = "Transmission type";
-                String conditioner = "Conditioner";
-                criteria.add(passengerAmount);
-                criteria.add(doorsAmount);
-                criteria.add(baggageAmount);
-                criteria.add(transmission);
-                criteria.add(conditioner);
+                List<String> criteria = searchCriteriaAvailableFields();
+//                String passengerAmount = "Passenger amount";
+//                String doorsAmount = "Doors amount";
+//                String baggageAmount = "Baggage amount";
+//                String transmission = "Transmission type";
+//                String conditioner = "Conditioner";
+//                criteria.add(passengerAmount);
+//                criteria.add(doorsAmount);
+//                criteria.add(baggageAmount);
+//                criteria.add(transmission);
+//                criteria.add(conditioner);
                 while (addAnotherCriteria) {
-                    System.out.println("Add another criteria?");
-                    System.out.println("1. Yes");
-                    System.out.println("2. No");
+                    System.out.println("""
+                            Add another criteria for search?
+                            1. Yes");
+                            2. No""");
                     int userChoice = Integer.parseInt(scanner.nextLine());
                     if (userChoice == 1) {
 
@@ -63,35 +58,35 @@ public class SearchVehicleUIAction implements UIAction {
                         int criteriaChoice = Integer.parseInt(scanner.nextLine());
 
                         switch (criteria.get(criteriaChoice - 1)) {
+                            case "Transmission type" -> {
+                                System.out.println("Enter transmission type (manual, automatic, none): ");
+                                String transmissionType = scanner.nextLine();
+                                searchVehicleRequestBuilder.transmissionType(transmissionType);
+                                criteria.remove(criteriaChoice - 1);
+                            }
                             case "Passenger amount" -> {
                                 System.out.println("Enter number of passengers (1-5): ");
                                 Integer numberOfPassengers = Integer.parseInt(scanner.nextLine());
                                 searchVehicleRequestBuilder.passengerAmount(numberOfPassengers);
-                                criteria.remove(passengerAmount);
+                                criteria.remove(criteriaChoice - 1);
                             }
                             case "Doors amount" -> {
                                 System.out.println("Enter number of doors (3-5): ");
                                 Integer numberOfDoors = Integer.parseInt(scanner.nextLine());
                                 searchVehicleRequestBuilder.doorsAmount(numberOfDoors);
-                                criteria.remove(doorsAmount);
+                                criteria.remove(criteriaChoice - 1);
                             }
                             case "Baggage amount" -> {
                                 System.out.println("Enter number of baggage (0-5): ");
                                 Integer numberOfBaggage = Integer.parseInt(scanner.nextLine());
                                 searchVehicleRequestBuilder.baggageAmount(numberOfBaggage);
-                                criteria.remove(baggageAmount);
-                            }
-                            case "Transmission type" -> {
-                                System.out.println("Enter transmission type (manual, automatic, none): ");
-                                String transmissionType = scanner.nextLine();
-                                searchVehicleRequestBuilder.transmissionType(transmissionType);
-                                criteria.remove(transmission);
+                                criteria.remove(criteriaChoice - 1);
                             }
                             case "Conditioner" -> {
                                 System.out.println("Has conditioner(true or false): ");
                                 String hasConditioner = scanner.nextLine();
                                 searchVehicleRequestBuilder.hasConditioner(hasConditioner);
-                                criteria.remove(conditioner);
+                                criteria.remove(criteriaChoice - 1);
                             }
                         }
                     }
@@ -124,5 +119,23 @@ public class SearchVehicleUIAction implements UIAction {
 
     }
 
+    private void printVehicleTypesMenu() {
+        System.out.println("""
+                Chose vehicle type:
+                1. Passenger Car
+                2. Mini Bus"
+                3. Motorcycle"
+                4. Car Trailer""");
+        System.out.println();
+    }
+
+    private List<String> searchCriteriaAvailableFields() {
+        return new ArrayList<>(
+                List.of("Transmission type",
+                        "Passenger amount",
+                        "Doors amount",
+                        "Baggage amount",
+                        "Conditioner"));
+    }
 
 }
