e66d0a530cfa8fd588695ba856ec156972d6d732
ArtursMel <melnikov270@gmail.com>
2022-10-02 23:07:27 +0300

    AM_team_im tests

--- team_internet_meme/src/test/java/lv/javaguru/java2/cookingApp/services/AddRecipeServiceTest.java
+++ team_internet_meme/src/test/java/lv/javaguru/java2/cookingApp/services/AddRecipeServiceTest.java
@@ -1,9 +1,9 @@
 package lv.javaguru.java2.cookingApp.services;
 
 import lv.javaguru.java2.cookingApp.database.Database;
+import lv.javaguru.java2.cookingApp.domain.Recipe;
 import lv.javaguru.java2.cookingApp.requests.AddRecipeRequest;
 import lv.javaguru.java2.cookingApp.responses.AddRecipeResponse;
-import lv.javaguru.java2.cookingApp.responses.CoreError;
 import lv.javaguru.java2.cookingApp.services.validators.AddRecipeRequestValidator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,6 +12,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,8 +20,11 @@ import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(MockitoExtension.class)
 class AddRecipeServiceTest {
 
-    @Mock private Database database;
-    @Mock private AddRecipeRequestValidator validator;
+    @Mock
+    private Database database;
+
+    @Mock
+    private AddRecipeRequestValidator validator;
 
     @InjectMocks
     private AddRecipeService service;
@@ -28,10 +32,20 @@ class AddRecipeServiceTest {
     @Test
     void testShouldReturnResponseWithErrorWhenRequestIsNotValid() {
         AddRecipeRequest request = Mockito.mock(AddRecipeRequest.class);
-        CoreError error = new CoreError("Error", "Error");
         AddRecipeResponse response = service.execute(request);
-
         assertTrue(response.hasErrors());
-
     }
+
+    @Test
+    void testShouldReturnResponseWithAddedRecipe() {
+        AddRecipeRequest request = Mockito.mock(AddRecipeRequest.class);
+        Mockito.when(validator.validate(request)).thenReturn(new ArrayList<>());
+
+        AddRecipeResponse response = service.execute(request);
+        assertFalse(response.hasErrors());
+        assertNotNull(response.getNewRecipe());
+        Recipe recipe = response.getNewRecipe();
+        Mockito.verify(database).save(recipe);
+    }
+
 }
