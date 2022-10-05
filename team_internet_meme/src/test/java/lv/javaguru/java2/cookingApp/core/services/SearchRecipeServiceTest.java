f245af42048ed75913c8586b94641e4e74873a68
ArtursMel <melnikov270@gmail.com>
2022-10-05 19:44:02 +0300

    AM_team_im tests

--- team_internet_meme/src/test/java/lv/javaguru/java2/cookingApp/core/services/SearchRecipeServiceTest.java
+++ team_internet_meme/src/test/java/lv/javaguru/java2/cookingApp/core/services/SearchRecipeServiceTest.java
@@ -1,9 +1,12 @@
 package lv.javaguru.java2.cookingApp.core.services;
 
 import lv.javaguru.java2.cookingApp.core.database.Database;
+import lv.javaguru.java2.cookingApp.core.domain.Recipe;
 import lv.javaguru.java2.cookingApp.core.requests.SearchRecipeRequest;
 import lv.javaguru.java2.cookingApp.core.responses.CoreError;
 import lv.javaguru.java2.cookingApp.core.responses.SearchRecipeResponse;
+import lv.javaguru.java2.cookingApp.core.services.search_criteria.AndSearchCriteria;
+import lv.javaguru.java2.cookingApp.core.services.search_criteria.SearchCriteria;
 import lv.javaguru.java2.cookingApp.core.services.validators.SearchRecipeRequestValidator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,10 +15,11 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 
 @ExtendWith(MockitoExtension.class)
 class SearchRecipeServiceTest {
@@ -37,24 +41,23 @@ class SearchRecipeServiceTest {
         assertEquals("Test", error.getMessage());
     }
 
-//    @Test
-//    void testShouldReturnResponseWithListOfRecipes() {
-//        SearchRecipeRequest request = Mockito.mock(SearchRecipeRequest.class);
-//        Recipe recipe1 = Mockito.mock(Recipe.class);
-//        Recipe recipe2 = Mockito.mock(Recipe.class);
-//        SearchCriteria searchCriteria = new AndSearchCriteria(null, null);
-//        Mockito.when(validator.validate(request)).thenReturn(new ArrayList<>());
-//        Mockito.when(database.find(searchCriteria)).thenReturn(List.of(recipe1, recipe2));
-//
-//        SearchRecipeResponse response = service.execute(request);
-//        assertNotNull(response.getRecipes());
-//
-//        assertFalse(response.hasErrors());
-//        assertEquals(recipe1, response.getRecipes().get(0));
-//        assertEquals(recipe2, response.getRecipes().get(1));
-//
-//        Mockito.verify(database).find(searchCriteria);
-//    }
-//
+    @Test
+    void testShouldReturnResponseWithListOfRecipes() {
+        SearchRecipeRequest request = new SearchRecipeRequest(List.of("Ingredient1","Ingredient2"));
+        Recipe recipe1 = Mockito.mock(Recipe.class);
+        Recipe recipe2 = Mockito.mock(Recipe.class);
+        Mockito.when(validator.validate(request)).thenReturn(new ArrayList<>());
+        Mockito.when(database.find(any())).thenReturn(List.of(recipe1, recipe2));
+
+        SearchRecipeResponse response = service.execute(request);
+        assertNotNull(response.getRecipes());
+
+        assertFalse(response.hasErrors());
+        assertEquals(recipe1, response.getRecipes().get(0));
+        assertEquals(recipe2, response.getRecipes().get(1));
+
+        Mockito.verify(database).find(any());
+    }
+
 
 }
