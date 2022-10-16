3b0d9b5e8d539c3416c5a465fec2a1f8b4832194
jevgenijs_tolks <yevgeniy.tolks@gmail.com>
2022-10-16 22:09:57 +0300

    lesson4 appointment/client search tests  commit v0.23

---
+++ team_eBooking/src/test/java/lv/javaguru/java2/eBooking/core/services/appointment/search/AppointmentSearchRequestValidatorTest.java
@@ -0,0 +1,48 @@
+package lv.javaguru.java2.eBooking.core.services.appointment.search;
+
+import lv.javaguru.java2.eBooking.core.requests.appointment_request.SearchAppointmentRequest;
+import lv.javaguru.java2.eBooking.core.responses.CoreError;
+import lv.javaguru.java2.eBooking.core.services.appointment.add.AppointmentValidationResult;
+import lv.javaguru.java2.eBooking.core.services.client.add.ClientValidationResult;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class AppointmentSearchRequestValidatorTest {
+
+    AppointmentSearchRequestValidator validator = new AppointmentSearchRequestValidator();
+    @Test
+    public void shouldNotReturnErrorWhenMasterNameIsProvided() {
+        SearchAppointmentRequest request = new SearchAppointmentRequest("Master name: ", null);
+        List<CoreError> errors = validator.validate(request);
+        assertEquals(errors.size(),0);
+    }
+
+    @Test
+    public void shouldNotReturnErrorWhenTypeOfServiceIsProvided() {
+        SearchAppointmentRequest request = new SearchAppointmentRequest(null, "Type of service: ");
+        List<CoreError> errors = validator.validate(request);
+        assertEquals(errors.size(),0);
+    }
+
+    @Test
+    public void shouldNotReturnErrorWhenMasterNameAndTypeOfServiceAreProvided() {
+        SearchAppointmentRequest request = new SearchAppointmentRequest("Master name: ", "Type of service: ");
+        List<CoreError> errors = validator.validate(request);
+        assertEquals(errors.size(),0);
+    }
+
+    @Test
+    public void shouldReturnErrorWhenMasterNameAndTypeOfServiceAreNotProvided() {
+        SearchAppointmentRequest request = new SearchAppointmentRequest(null, null);
+        List<CoreError> errors = validator.validate(request);
+        assertEquals(errors.size(),2);
+        assertEquals(errors.get(0).getField(),"Master name: ");
+        assertEquals(errors.get(0).getAppointmentValidationMessage(), AppointmentValidationResult.MASTERNAME_MUST_NOT_BE_EMPTY);
+        assertEquals(errors.get(1).getField(), "Type of service: ");
+        assertEquals(errors.get(1).getAppointmentValidationMessage(),AppointmentValidationResult.SERVICETYPE_MUST_NOT_BE_EMPTY);
+    }
+}
