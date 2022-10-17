cb0aaee97f6a30489739d83092714a5b70f42335
VladKuli <goldydefault@gmail.com>
2022-10-17 23:41:23 +0200

    eBank (SQL)

---
+++ team_eBank/database/dbDataInsertScript.sql
@@ -0,0 +1,8 @@
+insert into accounts(balance)
+values (10000);
+
+insert into bank_accounts(name,surname,personal_code)
+values ("Vladislav","Kulikov", "000000-00002");
+
+insert into users(personal_code,password)
+values ("000000-00002", "user");
