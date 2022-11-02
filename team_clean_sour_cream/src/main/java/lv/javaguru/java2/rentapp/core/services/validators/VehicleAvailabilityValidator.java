546bf090cdd72f757b7419c4c6f6d50e8dd95348
ArtursMel <melnikov270@gmail.com>
2022-11-02 18:22:31 +0200

    AM-csc - car rent search

---
+++ team_clean_sour_cream/src/main/java/lv/javaguru/java2/rentapp/core/services/validators/VehicleAvailabilityValidator.java
@@ -0,0 +1,11 @@
+package lv.javaguru.java2.rentapp.core.services.validators;
+
+import lv.javaguru.java2.rentapp.core.requests.GeneralRentVehicleRequest;
+import lv.javaguru.java2.rentapp.core.responses.CoreError;
+import org.apache.commons.validator.routines.DateValidator;
+
+import java.util.Optional;
+
+public class VehicleAvailabilityValidator {
+
+}
