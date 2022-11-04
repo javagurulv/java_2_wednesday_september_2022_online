b175ac3536a342bcccd243ae4dcdc1fafa0266bf
JB <jb@jb.jb>
2022-11-04 19:28:45 +0200

    SQL db creation and test data insertion scripts

---
+++ team_tasks_scheduler/database/dbDataInsertScript.sql
@@ -0,0 +1,29 @@
+INSERT INTO users (username, user_password, email, send_reminder)
+VALUES ('1111', 'b59c67bf196a4758191e42f76670ceba', 'a@b.c', false);
+
+INSERT INTO users (username, user_password, email, send_reminder)
+VALUES ('2222', '934b535800b1cba8f96a5d72f72f1611', 't@t.t', false);
+
+INSERT INTO users (username, user_password, email, send_reminder)
+VALUES ('John Doe', '934b535800b1cba8f96a5d72f72f1611', 'c@b.a', false);
+
+INSERT INTO users (username, user_password, email, send_reminder)
+VALUES ('Jane Doe', '2be9bd7a3434f7038ca27d1918de58bd', 'qwery@abc.hh', false);
+
+INSERT INTO users (username, user_password, email, send_reminder)
+VALUES ('Thor Odinson', 'dbc4d84bfcfe2284ba11beffb853a8c4', 'abc123@in.xz', false);
+
+INSERT INTO users (username, user_password, email, send_reminder)
+VALUES ('123soldier', '6074c6aa3488f3c2dddff2a7ca821aab', 'must@year.ac', false);
+
+INSERT INTO users (username, user_password, email, send_reminder)
+VALUES ('007Bond', 'e9510081ac30ffa83f10b68cde1cac07', 'james@adam.al', false);
+
+INSERT INTO users (username, user_password, email, send_reminder)
+VALUES ('Nagibator666', 'd79c8788088c2193f0244d8f1f36d2db', 'bender6@box.at', false);
+
+INSERT INTO users (username, user_password, email, send_reminder)
+VALUES ('Star Destroyer', 'cf79ae6addba60ad018347359bd144d2', 'creater@vava.one', false);
+
+INSERT INTO users (username, user_password, email, send_reminder)
+VALUES ('Darth Vader', 'fa246d0262c3925617b0c72bb20eeb1d', 'anakin@walker.sky', false);
--- team_tasks_scheduler/src/main/java/lv/javaguru/java2/tasksScheduler/console_ui/ExitUIAction.java
+++ team_tasks_scheduler/src/main/java/lv/javaguru/java2/tasksScheduler/console_ui/ExitUIAction.java
@@ -26,6 +26,7 @@ public class ExitUIAction implements UIAction {
             return false;
         } else {
             System.out.println("Good bye!");
+
             System.exit(0);
             return true;
         }
