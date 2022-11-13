337fe699d40f71a3b17a750a58ffd521f08b5d79
Yevgeniy <yevgeniy.tolks@gmail.com>
2022-11-13 15:53:29 +0200

    springbootapplication startup

---
+++ team_eBooking/src/main/java/lv/javaguru/java2/eBooking/WebBookingAppointmentApplication.java
@@ -0,0 +1,27 @@
+package lv.javaguru.java2.eBooking;
+
+import lv.javaguru.java2.eBooking.config.SpringWebConfiguration;
+import lv.javaguru.java2.eBooking.console_ui.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@RequestMapping
+public class WebBookingAppointmentApplication {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(SpringWebConfiguration.class);
+
+        ProgramMenu programMenu = context.getBean(ProgramMenu.class);
+
+        while (true) {
+            programMenu.printMenu();
+            int menuNumber = programMenu.chooseMenuNumber();
+            programMenu.executeSelectedMenuNumber(menuNumber);
+        }
+    }
+
+}
