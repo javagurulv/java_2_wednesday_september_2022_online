247cd2076f1bd18f846add2deb11b577c66cb333
ArtursMel <melnikov270@gmail.com>
2022-11-16 19:09:15 +0200

    AM-intMeme_web

--- team_internet_meme/src/test/java/lv/javaguru/java2/cookingApp/integrationtests/SpringContextTest.java
+++ team_internet_meme/src/test/java/lv/javaguru/java2/cookingApp/integrationtests/SpringContextTest.java
@@ -1,25 +1,33 @@
 package lv.javaguru.java2.cookingApp.integrationtests;
 
 import lv.javaguru.java2.cookingApp.config.SpringCoreConfiguration;
+import lv.javaguru.java2.cookingApp.web_ui.config.SpringWebConfiguration;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = {SpringCoreConfiguration.class})
+@ContextConfiguration(classes = {SpringWebConfiguration.class})
 public class SpringContextTest {
 
-    @Autowired
-    private ApplicationContext appContext;
+    private ConfigurableApplicationContext context;
+
+    @BeforeEach
+    void setup() {
+        context = SpringApplication.run(SpringWebConfiguration.class);
+    }
 
     @Test
     public void start() {
-        assertNotNull(appContext);
+        assertNotNull(context);
     }
 
 
