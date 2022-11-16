db09badad334d7c3b217b5b94050038cc3718d7d
dmitry-mak <akkaru@gmail.com>
2022-11-16 12:50:19 +0200

    MySql database integrated

--- team_atm_system/src/main/java/generalPackage/core/database/AccountDatabaseImpl.java
+++ team_atm_system/src/main/java/generalPackage/core/database/AccountDatabaseImpl.java
@@ -8,8 +8,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-@Component
-public class AccountDatabaseImpl implements Database {
+//@Component
+ public class AccountDatabaseImpl implements Database {
 
     private List<Accounts> accounts = new ArrayList<>();
 
---
+++ team_atm_system/src/main/java/generalPackage/core/database/AccountsRowMapper.java
@@ -0,0 +1,20 @@
+package generalPackage.core.database;
+
+import generalPackage.Accounts;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AccountsRowMapper implements RowMapper<Accounts> {
+
+    @Override
+    public Accounts mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+        Accounts accounts = new Accounts();
+        accounts.setUserID(resultSet.getInt("id"));
+        accounts.setName(resultSet.getString("name"));
+        accounts.setBalance(resultSet.getInt("balance"));
+
+        return accounts;
+    }
+}
