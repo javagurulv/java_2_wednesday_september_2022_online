9f055e8da945e9e893ff3bcb4bae94795d6ca1b3
JB <jb@jb.jb>
2022-12-26 23:01:15 +0200

    liquibase added

--- team_tasks_scheduler/src/main/java/lv/javaguru/java2/tasksScheduler/config/TaskSchedulerCoreConfig.java
+++ team_tasks_scheduler/src/main/java/lv/javaguru/java2/tasksScheduler/config/TaskSchedulerCoreConfig.java
@@ -1,8 +1,7 @@
 package lv.javaguru.java2.tasksScheduler.config;
 
-import org.hibernate.SessionFactory;
-import org.apache.commons.dbcp2.BasicDataSource;
-import org.springframework.beans.factory.annotation.Value;
+
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
@@ -10,15 +9,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
-import java.io.IOException;
-import java.util.Properties;
 
 @Configuration
 @EnableAutoConfiguration
@@ -28,5 +21,14 @@ import java.util.Properties;
 @EntityScan(basePackages = "lv.javaguru.java2.tasksScheduler.core.domain")
 @EnableJpaRepositories(value = "lv.javaguru.java2.tasksScheduler.core.database.jpa")
 public class TaskSchedulerCoreConfig {
+
+    @Bean
+    public SpringLiquibase springLiquibase(DataSource dataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:/db/changelog/changelog-master.xml");
+        liquibase.setShouldRun(true);
+        liquibase.setDataSource(dataSource);
+        return liquibase;
+    }
 
 }
