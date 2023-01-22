0cc4da53a9bb05329c57b66d20c83f4359cebaf3
Olegs-github <oleg-k@inbox.lv>
2023-01-22 07:51:02 +0200

    Minor changes in SQL scripts

--- team_tasks_scheduler/src/main/java/lv/javaguru/java2/tasksScheduler/utils/WebUI.java
+++ team_tasks_scheduler/src/main/java/lv/javaguru/java2/tasksScheduler/utils/WebUI.java
@@ -5,7 +5,7 @@ public class WebUI {
     public static String getGreeting(String name) {
         String greeting;
         if (ValueChecking.stringIsEmpty(name)) {
-            greeting = "Welcome in the system!";
+            greeting = "Welcome to Your account!";
         } else {
             greeting = "Welcome, " + name + "!";
         }
