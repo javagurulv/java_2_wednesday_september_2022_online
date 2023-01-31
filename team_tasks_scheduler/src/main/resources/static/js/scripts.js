6de67e0dc3134f33a91dd47241e8d725102912ac
JB <jb@jb.jb>
2023-01-31 22:54:29 +0200

    add user web page now uses REST

--- team_tasks_scheduler/src/main/resources/static/js/scripts.js
+++ team_tasks_scheduler/src/main/resources/static/js/scripts.js
@@ -1,5 +1,59 @@
+function registerUser(event) {
+    event.preventDefault();
+    document.getElementById("msg-error").style.display = "none";
+    document.getElementById("msg-succeed").style.display = "none";
 
-	function getUsersNames (event) {
+
+    send().then((dataObj) => {
+        _data = dataObj.data;
+        _hasErrors = dataObj.hasErrors;
+        let text = "";
+        if (_hasErrors == false) {
+            console.log(_data.message);
+            _data.forEach(msg => {
+                text += msg.message;
+            })
+            document.getElementById("msg-succeed").style.display = "initial";
+            document.getElementById("msg-succeed").innerHTML =  text;
+        }
+        else {
+            _data.forEach(error => {
+                console.log(error.error + ": " + error.msg);
+                text += error.error + ": " + error.msg + "<br>";
+            })
+            document.getElementById("msg-error").style.display = "initial";
+            document.getElementById("msg-error").innerHTML =  text;
+        }
+    })
+}
+
+async function send() {
+    const userRegistrationForm = document.getElementById("userInfoInput");
+    const FD = new FormData(userRegistrationForm);
+
+    const baseUrl = "http://localhost:8080/api/registerUser";
+
+    const userInfo = {username:FD.get("username"), userPassword:FD.get("userPassword"),
+                        userEmail:FD.get("userEmail"), sendReminders:FD.get("sendReminders")};
+
+    const response = await fetch(baseUrl, {method:"POST",
+    							            headers:{"Content-Type":"application/json",
+    							                            "Accept":"application/json"},
+    							            body:JSON.stringify(userInfo)});
+    let data, hasErrors;
+    if (response.status == 200) {
+        data = await response.json();
+    	hasErrors = false
+    }
+    else if (response.status == 400) {
+        data = await response.json()
+    	hasErrors = true;
+    }
+
+    return {data, hasErrors};
+}
+
+function getUsersNames (event) {
 		event.preventDefault();
 		get().then((dataObj) => {
 			_data = dataObj.data;
@@ -24,30 +78,29 @@
 			}
 		})
 	}
-	async function get() {
+async function get() {
+    const userNameQueryForm = document.getElementById("userQueryForm");
+	const FD = new FormData(userNameQueryForm);
 
-		const userNameQueryForm = document.getElementById("userQueryForm");
-		const FD = new FormData(userNameQueryForm);
+    const baseUrl = `http://localhost:8080/api/getUsersNames`;
+	let namedUrl = baseUrl+"/"+FD.get("username");
+	const viewParameters = {ordering:FD.get("orderDirection"), pageNumber:FD.get("pageNumber"), pageSize:FD.get("recordOnPage")};
 
-        const baseUrl = `http://localhost:8080/api/getUsersNames`;
-		let namedUrl = baseUrl+"/"+FD.get("username");
-		const object = {ordering:FD.get("orderDirection"), pageNumber:FD.get("pageNumber"), pageSize:FD.get("recordOnPage")};
-
-		const response = await fetch(namedUrl, {method:"POST",
+	const response = await fetch(namedUrl, {method:"POST",
 							headers:{"Content-Type":"application/json", "Accept":"application/json"},
-							body:JSON.stringify(object)})
-		let data, hasErrors;
-		if (response.status == 200) {
-			data = await response.json();
-			hasErrors = false
-		}
-		else if (response.status == 400) {
-			data = await response.json()
-			hasErrors = true;
-		}
-
-		return {data, hasErrors};
+							body:JSON.stringify(viewParameters)})
+	let data, hasErrors;
+	if (response.status == 200) {
+		data = await response.json();
+		hasErrors = false
 	}
+	else if (response.status == 400) {
+		data = await response.json()
+		hasErrors = true;
+	}
+
+	return {data, hasErrors};
+}
 
 
     async function getUsersNames2(event) {
