214c4fd1b0d33e3a8c416b296e1d41c62246994c
JB <jb@jb.jb>
2023-02-03 22:39:22 +0200

    add task page rework; date-time field split to date+time fields

--- team_tasks_scheduler/src/main/java/lv/javaguru/java2/tasksScheduler/web_ui/controllers/rest/AddTaskRESTController.java
+++ team_tasks_scheduler/src/main/java/lv/javaguru/java2/tasksScheduler/web_ui/controllers/rest/AddTaskRESTController.java
@@ -14,7 +14,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -33,19 +35,9 @@ public class AddTaskRESTController {
     @PostMapping(consumes = "application/json", produces = "application/json")
     ResponseEntity<List<Map<String,String>>> processAddTaskRequest(@RequestBody Map<String,String> newTaskInfo) {
 
-        LocalDateTime dueDate;
-        LocalDateTime endDate;
+        LocalDateTime dueDate = getDateFromInputData(newTaskInfo, "dueDate");
+        LocalDateTime endDate = getDateFromInputData(newTaskInfo, "endDate");
         int regularity = parseInt(newTaskInfo.get("regularity"));
-        try {
-            dueDate = LocalDateTime.parse(newTaskInfo.get("dueDate"));
-        } catch (RuntimeException e) {
-            dueDate = LocalDateTime.of(1973,01,01,1,1);
-        }
-        try {
-            endDate = LocalDateTime.parse(newTaskInfo.get("endDate"));
-        } catch (RuntimeException e) {
-            endDate = LocalDateTime.of(1973, 01, 01, 1, 1);
-        }
 
         AddTaskRequest request = new AddTaskRequest(newTaskInfo.get("taskDescription"), regularity,
                                                                                 dueDate, endDate);
@@ -79,6 +71,31 @@ public class AddTaskRESTController {
             errorMapList.add(errorMap);
         }
         return errorMapList;
+    }
+    LocalDateTime getDateFromInputData(Map<String,String> inputData, String identifier) {
+        LocalDateTime dateTime = LocalDateTime.now();
+
+        if (identifier.equals("dueDate")) {
+            try {
+                LocalDate date = LocalDate.parse(inputData.get("dueDate"));
+                LocalTime time = LocalTime.parse(inputData.get("dueTime"));
+                dateTime = LocalDateTime.of(date, time);
+            } catch (RuntimeException e) {
+                //set unacceptable date to get error from validator
+                dateTime = LocalDateTime.of(1973, 01, 01, 1, 1);
+            }
+        }
+        else if (identifier.equals("endDate")) {
+            try {
+                LocalDate date = LocalDate.parse(inputData.get("endDate"));
+                LocalTime time = LocalTime.parse(inputData.get("endTime"));
+                dateTime = LocalDateTime.of(date, time);
+            } catch (RuntimeException e) {
+                //set unacceptable date to get error from validator
+                dateTime = LocalDateTime.of(1973, 01, 01, 1, 1);
+            }
+        }
+        return dateTime;
     }
 
 }
