19502f5c847ab77a9984b9173073420af69f4ff1
Aleksandr <korsaks.aleksandrs@gmail.com>
2023-02-24 19:18:15 +0200

    team_csc client service in progress...

--- team_clean_sour_cream/src/test/java/lv/javaguru/java2/rentapp/core/services/validators/ExistedClientCheckValidatorTest.java
+++ team_clean_sour_cream/src/test/java/lv/javaguru/java2/rentapp/core/services/validators/ExistedClientCheckValidatorTest.java
@@ -40,14 +40,26 @@ class ExistedClientCheckValidatorTest {
     void first() {
         Mockito.when(exClientChkService.checkClientExistenceByPersonalId(addClientRequest)).thenReturn(Optional.of(1L));
         Mockito.when(exClientChkService.checkClientExistenceByEmail(addClientRequest)).thenReturn(Optional.of(2L));
-//        Mockito.when(clientDatabase.getById(Mockito.any())).thenReturn(Optional.of(client));
-//        Mockito.when(client.getFirstName()).thenReturn("a");
-//        Mockito.when(client.getLastName()).thenReturn("b");
-//        Mockito.when(addClientRequest.)
         List<CoreError> errors = existedClientCheckValidator.validate(addClientRequest);
         assertEquals(1, errors.size());
         assertEquals("Personal ID and Email", errors.get(0).getField());
         assertEquals("already exist in other different client`s records", errors.get(0).getMessage());
+    }
+
+    @Test
+    void second() {
+        Mockito.when(exClientChkService.checkClientExistenceByPersonalId(addClientRequest)).thenReturn(Optional.of(2L));
+        Mockito.when(exClientChkService.checkClientExistenceByEmail(addClientRequest)).thenReturn(Optional.of(2L));
+        Mockito.when(clientDatabase.getById(Mockito.any())).thenReturn(Optional.of(client));
+        Mockito.when(client.getFirstName()).thenReturn("a");
+        Mockito.when(client.getLastName()).thenReturn("b");
+        Mockito.when(addClientRequest.getFirstName()).thenReturn("a1");
+        Mockito.when(addClientRequest.getLastName()).thenReturn("b");
+        List<CoreError> errors = existedClientCheckValidator.validate(addClientRequest);
+        assertEquals(1, errors.size());
+        assertEquals("First Name", errors.get(0).getField());
+        assertEquals("differs from the first name of existing client record(s) with same personal ID and/or Email",
+                errors.get(0).getMessage());
     }
 
 //    @Test
