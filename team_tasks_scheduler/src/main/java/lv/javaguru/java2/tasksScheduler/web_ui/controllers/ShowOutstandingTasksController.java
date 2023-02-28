1608943077b1b1d577c959e55df176c4781c6fdd
JB <jb@jb.jb>
2023-02-28 22:44:11 +0200

    show outstanding tasks, show tasks for today web-page created.

---
+++ team_tasks_scheduler/src/main/java/lv/javaguru/java2/tasksScheduler/web_ui/controllers/ShowOutstandingTasksController.java
@@ -0,0 +1,31 @@
+package lv.javaguru.java2.tasksScheduler.web_ui.controllers;
+
+import lv.javaguru.java2.tasksScheduler.core.requests.GetCurrentUserRequest;
+import lv.javaguru.java2.tasksScheduler.core.responses.GetCurrentUserResponse;
+import lv.javaguru.java2.tasksScheduler.core.services.system.GetCurrentUserService;
+import lv.javaguru.java2.tasksScheduler.utils.WebUI;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class ShowOutstandingTasksController {
+
+    @Autowired
+    private GetCurrentUserService getCurrentUserService;
+
+    @GetMapping(value = "/showOutstandingTasks")
+    public String showOutstandingTasks(ModelMap modelMap) {
+        GetCurrentUserRequest request = new GetCurrentUserRequest(true);
+        GetCurrentUserResponse response = getCurrentUserService.execute(request);
+        if (response.hasErrors()) {
+            modelMap.addAttribute("errors", response.getErrors());
+        } else {
+            modelMap.addAttribute("greeting", WebUI.getGreeting(response.getUser().getUsername()));
+            modelMap.addAttribute("request", response.getUser());
+        }
+        return "showOutstandingTasks";
+    }
+}
